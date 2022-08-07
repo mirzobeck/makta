a332e598aba108f6891d2a25374f988deda205c1
mirzobeck <99405198+mirzobeck@users.noreply.github.com>
2022-08-07 09:46:03 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -5,5 +5,5 @@ env = Env()
 env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = "5236087618:AAHBxV4b23kWDhkiHRXsPue6eyMntihco9I"  # Bot toekn
-ADMINS = [1361526026] # adminlar ro'yxati
+BOT_TOKEN = "Bot tokeni"  # Bot toekn
+ADMINS = [Adminning telegram id] # adminlar ro'yxati
